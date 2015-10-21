08f4197f57487c449131bd434b85665865cbc2ae
Páblo <pablo.santos@ccc.ufcg.edu.br>
2015-10-21 20:47:57 -0300

    Criado a classe User

---
+++ app/src/main/java/com/example/huawei/easytrade/Model/User.java
@@ -0,0 +1,67 @@
+package com.example.huawei.easytrade.Model;
+
+/**
+ * Created by huawei on 21/10/15.
+ */
+public class User {
+
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+    private String nickname;
+    private String photo;
+
+    public User() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+}
